6280c466ca9939db2a895dc88a3d4222b9ce31ac
David Yang <david.gfx@gmail.com>
2023-02-24 09:13:38 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,225 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./breadcalc.png";
+import { recipes } from "./recipes";
+import {
+  TextField,
+  Select,
+  MenuItem,
+  Typography,
+  Grid,
+  Box,
+  Stack,
+  Container,
+} from "@mui/material";
 
 function App() {
+  const [flourWeight, setFlourWeight] = useState("");
+  const [saltWeight, setSaltWeight] = useState("");
+  const [fatWeight, setFatWeight] = useState("");
+  const [waterPercentage, setWaterPercentage] = useState("");
+  const [waterWeight, setWaterWeight] = useState("");
+  const [saltPercentage, setSaltPercentage] = useState("");
+  const [fatPercentage, setFatPercentage] = useState("");
+  const [doughWeight, setDoughWeight] = useState("", [
+    flourWeight,
+    waterWeight,
+    waterPercentage,
+    saltWeight,
+    saltPercentage,
+    fatWeight,
+    fatPercentage,
+  ]);
+
+  const handlePresetChange = (event) => {
+    const recipe = recipes.filter((bread) => event === bread.breadType)[0];
+    setWaterPercentage(recipe.liquid * 100);
+    setSaltPercentage(recipe.salt * 100);
+    setFatPercentage(recipe.fat * 100);
+    setWaterWeight(recipe.liquid * flourWeight);
+    setSaltWeight(recipe.salt * flourWeight);
+    setFatWeight(recipe.fat * flourWeight);
+    setDoughWeight(
+      (
+        Number(flourWeight) +
+        Number(waterWeight) +
+        Number(saltWeight) +
+        Number(fatWeight)
+      ).toFixed(2)
+    );
+  };
+
+  const handleFlourWeightChange = (event) => {
+    if (event.target.value >= 0) {
+      setFlourWeight(event.target.value);
+      setWaterWeight((waterPercentage / 100) * flourWeight);
+      setSaltWeight((saltPercentage / 100) * flourWeight);
+      setFatWeight((fatPercentage / 100) * flourWeight);
+      setDoughWeight(
+        (
+          Number(flourWeight) +
+          Number(waterWeight) +
+          Number(saltWeight) +
+          Number(fatWeight)
+        ).toFixed(2)
+      );
+    }
+  };
+
+  const handleWaterPercentageChange = (event) => {
+    if (event.target.value >= 0) {
+      setWaterPercentage(event.target.value);
+      setWaterWeight((event.target.value / 100) * flourWeight);
+      setDoughWeight(
+        (
+          Number(flourWeight) +
+          Number(waterWeight) +
+          Number(saltWeight) +
+          Number(fatWeight)
+        ).toFixed(2)
+      );
+    }
+  };
+
+  const handleSaltPercentageChange = (event) => {
+    if (event.target.value >= 0) {
+      setSaltPercentage(event.target.value);
+      setSaltWeight((event.target.value / 100) * flourWeight);
+      setDoughWeight(
+        (
+          Number(flourWeight) +
+          Number(waterWeight) +
+          Number(saltWeight) +
+          Number(fatWeight)
+        ).toFixed(2)
+      );
+    }
+  };
+
+  const handleFatPercentageChange = (event) => {
+    if (event.target.value >= 0) {
+      setFatPercentage(event.target.value);
+      setFatWeight((event.target.value / 100) * flourWeight);
+      setDoughWeight(
+        (
+          Number(flourWeight) +
+          Number(waterWeight) +
+          Number(saltWeight) +
+          Number(fatWeight)
+        ).toFixed(2)
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container maxWidth="sm">
+        <Box textAlign={"center"} sx={{ mt: 5 }}>
+          <img src={logo} width="100px" alt="logo" />
+          <Typography fontFamily={"Homemade Apple"} variant="h5" sx={{ mb: 5 }}>
+            Bread Calculator
+          </Typography>
+        </Box>
+
+        <Stack
+          direction={"row"}
+          spacing={2}
+          sx={{ mb: 5 }}
+          alignItems={"center"}
+          justifyContent={"flex-end"}
         >
-          Learn React
-        </a>
-      </header>
+          <Typography fontFamily={"Shadows Into Light"} variant="h5">
+            Presets
+          </Typography>
+          <Select
+            sx={{ minWidth: 170 }}
+            size="small"
+            onChange={(e) => handlePresetChange(e.target.value)}
+          >
+            <MenuItem value="">None</MenuItem>
+            {recipes.map((recipe) => (
+              <MenuItem value={recipe.breadType}>{recipe.breadType}</MenuItem>
+            ))}
+          </Select>
+        </Stack>
+
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Flour (g)"
+              variant="outlined"
+              type="number"
+              fullWidth
+              value={flourWeight}
+              onChange={handleFlourWeightChange}
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              label="Water (g)"
+              variant="outlined"
+              type="number"
+              value={waterWeight}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              label="Water (%)"
+              variant="outlined"
+              type="number"
+              value={waterPercentage}
+              onChange={handleWaterPercentageChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              label="Salt (g)"
+              variant="outlined"
+              type="number"
+              value={saltWeight}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              label="Salt (%)"
+              variant="outlined"
+              type="number"
+              value={saltPercentage}
+              onChange={handleSaltPercentageChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              label="Fat (g)"
+              variant="outlined"
+              type="number"
+              value={fatWeight}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              label="Fat (%)"
+              variant="outlined"
+              type="number"
+              value={fatPercentage}
+              onChange={handleFatPercentageChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Dough Weight (g)"
+              variant="outlined"
+              fullWidth
+              value={doughWeight}
+            />
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
---
+++ src/recipes.js
@@ -0,0 +1,11 @@
+export const recipes = [
+  { breadType: "Neapolitan Pizza", liquid: 0.59, salt: 0.015, fat: 0 },
+  { breadType: "Baguette", liquid: 0.6, salt: 0.02, fat: 0 },
+  { breadType: "French Bread", liquid: 0.66, salt: 0.02, fat: 0 },
+  { breadType: "High Hydration", liquid: 0.75, salt: 0.03, fat: 0 },
+  { breadType: "Ciabatta", liquid: 0.8, salt: 0.02, fat: 0.025 },
+  { breadType: "Foccacia", liquid: 0.8, salt: 0.03, fat: 0.05 },
+  { breadType: "White Bread", liquid: 0.54, salt: 0.02, fat: .17 },
+  { breadType: "Challah", liquid: 0.52, salt: 0.02, fat: 0.25 },
+  { breadType: "Brioche", liquid: 0.22, salt: 0.02, fat: 1.33 },
+];
